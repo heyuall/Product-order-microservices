b7afbf571d9fe1b9e90763c2f6262e5fcafff0d9
Houssem <zoubeidihoussem@gmail.com>
2024-04-30 16:21:14 +0100

    Add integration test for product service application

--- product-servcie/src/test/java/com/houssem/microservices/productservcie/ProductServcieApplicationTests.java
+++ product-servcie/src/test/java/com/houssem/microservices/productservcie/ProductServcieApplicationTests.java
@@ -1,13 +1,55 @@
 package com.houssem.microservices.productservcie;
 
+import com.houssem.microservices.productservcie.dto.ProductRequest;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+import java.math.BigDecimal;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServcieApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.7");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(productRequest)
+				.when()
+				.post("/api/v1/product")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo(productRequest.name()))
+				.body("description", Matchers.equalTo(productRequest.description()))
+				.body("price", Matchers.is(productRequest.price().intValueExact()));
+	}
+
+	private ProductRequest getProductRequest() {
+		return new ProductRequest("iPhone 13", "iPhone 13", BigDecimal.valueOf(1200));
 	}
 
 }
